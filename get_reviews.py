89664807d03aec0ae3b7ca020b9669f69498feb5
JoanaBayraktar <123741642+JoanaBayraktar@users.noreply.github.com>
2023-01-27 14:51:52 +0100

    Create get_reviews.py

---
+++ get_reviews.py
@@ -0,0 +1,32 @@
+from google_play_scraper import Sort, reviews_all
+import pandas as pd
+from iso639 import languages
+
+df_all = pd.DataFrame(
+    columns=["reviewId", "userImage", "content	score", "thumbsUpCount", "reviewCreatedVersion", "at", "replyContent",
+             "repliedAt", "lang"])
+
+for i in range(len(list(languages.part1))):
+    review_language = list(languages.part1)[i]
+    print("Language: " + str(review_language))
+
+    result = reviews_all(
+        'de.rki.coronawarnapp',
+        sleep_milliseconds=9,
+        lang=review_language,
+        sort=Sort.MOST_RELEVANT)
+
+    df_res = pd.DataFrame(result)
+
+    for i in range(len(df_res)):
+        if df_res["reviewId"][i] in df_all['reviewId'].values:
+            df_res = df_res.drop([i])
+        else:
+            pass
+
+    for item in df_res.items():
+        df_res["lang"] = review_language
+
+    df_all = pd.concat([df_all, df_res])  # append is deprecated and will be removed from pandas in a future version
+
+df_all.to_csv("de.rki.coronawarnapp_rev.csv", sep='\t', encoding='utf-8')
